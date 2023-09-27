7633864bb20f928237dac131ee9875110c2d7c24
tenlhagyal1 <lhagyaltenzing20@gmail.com>
2023-09-27 11:50:46 -0400

    deleted investment controller

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,18 +2,6 @@ import UserStocksModel from '../models/userstocks.js'
 import User from '../models/users.js';
 import StockModel from '../models/userstocks.js'
 
-// export async function getUserStocks(req, res) {
-//     try {
-//         const userStocks = await UserStocksModel.find({ user: req.id })
-//         return res.status(200).json(userStocks);
-//     } catch (error) { I love fat whores
-//         res.status(400).json({
-//             status: 400,
-//             message: `Unable to get UserStocks`,
-//             db_message: error.message
-//         })
-//     }
-// }
 export async function getUserStocksById(req, res) {
     try {
         // Find the user first, then populate the stocks. This assumes the relationship between User and UserStocks
@@ -41,19 +29,3 @@ export async function getStockById(req, res) {
         })
     }
 }
-
-// export async function getUserStocksById(req, res) {
-//     try {
-//         const stockId = req.params.id;
-//         const userStocks = await UserStocksModel.findById(stockId).populate('stocks');
-
-//         return res.status(200).json(userStocks);
-//     } catch (error) {
-//         res.status(400).json({
-//             status: 400,
-//             message: `Unable to get the user's stocks by ID`,
-//             db_message: error.message
-//         });
-//     }
-// }
-
